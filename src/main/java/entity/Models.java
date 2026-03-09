cb1b38fdaeb5034fbca4ea80684884959d804364
ORION-VI <orion.digiworks@gmail.com>
2026-03-09 21:13:35 +0300

    Добавил модели автобусов. Впоследствии пригодится для рандомной генерации объектов.

---
+++ src/main/java/entity/Models.java
@@ -0,0 +1,22 @@
+package entity;
+
+public enum Models {
+    MERCEDES,
+    VOLVO,
+    MAN,
+    SCANIA,
+    GILLIG,
+    SOLARIS,
+    BYD,
+    NEOPLAN,
+    IRIZAR,
+    MITSUBISHI,
+    PROTERRA,
+    YUTONG,
+    CRRC,
+    ICARUS,
+    PAZ,
+    MAZ,
+    LIAZ,
+    LAZ
+}
